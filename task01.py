2b9980941821c62a61d5e24a25c4bf802da7bd6b
MarinaOrlova <oma119@yandex.ru>
2022-12-20 20:09:35 +0700

    task 01 summa

---
+++ task01.py
@@ -0,0 +1,14 @@
+# Задача 1
+# Напишите программу, которая принимает на вход вещественное число 
+# и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+number = input('Введите вещественное число: ')
+def summa (number):
+    summa = 0
+    for i in number:
+        if i != '.':
+            summa = summa + int(i)
+    return summa
+print (summa(number))
